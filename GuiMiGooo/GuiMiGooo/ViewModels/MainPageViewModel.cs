f9cc1a76092dfa31936ad5c06c019ae35392af94
Monthas <monthewansi.herve@gmail.com>
2020-11-20 11:00:47 +0100

    a bit of UI

---
+++ GuiMiGooo/GuiMiGooo/ViewModels/MainPageViewModel.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using Xamarin.Forms;
+
+namespace GuiMiGooo.ViewModels
+{
+    public class MainPageViewModel : INotifyPropertyChanged
+    {
+        public MainPageViewModel()
+        {
+            AllNotes = new ObservableCollection<string>();
+
+            EraseCommand = new Command(() =>
+            {
+                if (AllNotes.Contains(TheNote))
+                    AllNotes.Remove(theNote);
+
+                TheNote = string.Empty;
+            });
+
+            SaveCommand = new Command(() =>
+           {
+               if(!AllNotes.Contains(TheNote))
+                   AllNotes.Add(TheNote);
+                   
+               TheNote = string.Empty;
+
+           });
+
+        }
+
+        // public ObservableCollection<string> allNotes;
+        public ObservableCollection<string> AllNotes { get; }
+
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        string theNote;
+
+        public string TheNote
+        {
+
+            get => theNote;
+            set
+            {
+                theNote = value;
+
+                var args = new PropertyChangedEventArgs(nameof(TheNote));
+
+                PropertyChanged?.Invoke(this, args);
+            }
+        }
+
+        public Command SaveCommand { get; }
+        public Command EraseCommand { get; }
+
+
+    }
+}
